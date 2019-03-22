985d7f6adf749a88cc6cff25f2ba40ab75bd1776
Vladmvg <vladmvg24@gmail.com>
2019-03-22 22:36:59 +0300

    Add warmup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
+  if (temperature == 0) {
+		return "32";
+	} else if (temperature <= 0) {
+		fahrenheit = 32 - 1.8 * Math.abs(temperature);
+		return fahrenheit;
+	} else {
+		fahrenheit = 1.8 * temperature + 32;
+		return fahrenheit;
+	}
 };
+	var temperature;
+	var fahrenheit;
